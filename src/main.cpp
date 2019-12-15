9b8c64baf333736c9ed1a938d44d3cca2355d8c2
BlackTiger0815 <58547681+BlackTiger0815@users.noreply.github.com>
2019-12-15 11:26:20 +0100

    Parameter Handling
    Aufruf:
    ./src/MasterCube server <port>
    oder
    ./src/MasterCube client <IP-Adresse> <Port>
    
    Zusätzliche Erstellung der Files solver.h und solver.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,62 @@
 #include "cube.h"
 #include "debug.h"
-
-
+#include "solver.h" //Solver for Mastermind and Rubiks Cube
 
 int main (int argc, char* argv[]) {
 	
-	//new 13.12.2019 adding debug
+	//new 13.12.2019 adding debug by Thomas
 	debug Debugger;
 	Debugger.setdebug(0);
 	Debugger.setdebug(1);
 	Debugger.printf("MasterCube is starting");
 	// Debug inilasition over
+
+	//15.12.2019 handling of calling parameters by Isabella
+	// Port and IP-adress needs to be calculated from String for using it in communication 
+	// @Elisabeth -> bei mir melden wie dus brauchst ;)
+	string ServerIPAdress = "", ServerPort = "";
+	string modus = "";
 	
-	
-	
-	
+	if(argc == 1)
+		Debugger.printf("Not enough parameters");
+	else{
+		
+		//for(int i=0; i<argc; i++){
+		//	cout << i<< ":  " << argv[i] << endl;
+		//}
+		
+		if(string(argv[1]) == "client"){
+			modus = "client";
+			ServerIPAdress = argv[2];
+			ServerPort = argv[3];
+		}
+		else if(string(argv[1]) == "server"){
+			modus = "server";
+			ServerPort = argv[2]; 
+		}
+		else{
+			Debugger.printf("Wrong init-parameters");
+		}
+		
+	}
+	// parameter handling over
+
 	
 	Cube solved;
 	Cube scrambled;
 	
-/*	scrambled.scramble();
-	solved.printCube();
+	scrambled.scramble();
+/*	solved.printCube();
 	cout << "------------------" << endl<<endl;
 	scrambled.printCube();
-*/	
+
 	string s;
 	int n = 0;
 	cin >> n >> s;
 	scrambled.splitQuestion(s, n);
+*/
+	RubiksCubeSolver solver;
+	solver.calculateManhattanDistance(4);
 
     return 0;
 }
---
+++ src/solver.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+class RubiksCubeSolver
+{
+private:
+	int ManhattanDistance;
+	
+	
+public:
+	RubiksCubeSolver(){};
+	~RubiksCubeSolver(){};
+	
+	void calculateManhattanDistance(int x)
+	{
+		cout << x << endl;
+	}
+
+	
+};
