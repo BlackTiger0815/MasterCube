9978c259ce94dce56cd7f62450e953b65e6a2329
BlackTiger0815 <58547681+BlackTiger0815@users.noreply.github.com>
2019-12-16 15:10:09 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,45 @@
 #include <iostream>
 
 int main (int argc, char* argv[]) {
+	
+	//new 13.12.2019 adding debug by Thomas
+	debug Debugger;
+	Debugger.setdebug(0);
+	Debugger.setdebug(1);
+	Debugger.printf("MasterCube is starting");
+	// Debug inilasition over
+
+	//15.12.2019 handling of calling parameters by Isabella
+	// Port and IP-adress needs to be calculated from String for using it in communication 
+	// @Elisabeth -> bei mir melden wie dus brauchst ;)
+	string ServerIPAdress = "", ServerPort = "";
+	string modus = "";
+	
+	if(argc == 1)
+		Debugger.printf("Not enough parameters");
+	else{
+		
+		//for(int i=0; i<argc; i++){
+		//	cout << i<< ":  " << argv[i] << endl;
+		//}
+		
+		if(string(argv[1]) == "client"){
+			modus = "client";
+			ServerIPAdress = argv[2];
+			ServerPort = argv[3];
+		}
+		else if(string(argv[1]) == "server"){
+			modus = "server";
+			ServerPort = argv[2]; 
+		}
+		else{
+			Debugger.printf("Wrong init-parameters");
+		}
+		
+	}
+	// parameter handling over
+	
+	
 	Cube solved;
 	Cube scrambled;
 
